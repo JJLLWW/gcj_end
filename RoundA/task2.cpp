2eefece1b488fe5e7f10d225af3bbf681e5f4053
Jack Wright <jackwright@Jacks-MacBook-Pro.local>
2023-04-19 21:17:24 +0100

    forgot to commit

--- RoundA/task2.cpp
+++ RoundA/task2.cpp
@@ -1,11 +1,33 @@
-#include <cstring>
-#include <filesystem>
 #include <iostream>
-#include <stdio.h>
-#include <string>
 
 #ifdef DEBUG_HACK
 #include "debug_hack.hpp"
 #endif
 
-int main(int argc, char **argv) { ; }
+class case_loop {
+public:
+  case_loop() = default;
+  void read_and_solve() {
+    std::cin >> T;
+    for (int t = 1; t <= T; ++t) {
+      read();
+      solve();
+      std::cout << "Case #" << t << ":";
+    }
+  }
+
+private:
+  void read() { ; }
+  void solve() { ; }
+
+private:
+  int T;
+};
+
+int main(int argc, char **argv) {
+#ifdef DEBUG_HACK
+  dh::debug_hack(argv);
+#endif
+  case_loop main_loop;
+  main_loop.read_and_solve();
+}
--- RoundA/task4.cpp
+++ RoundA/task4.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 
+#ifdef DEBUG_HACK
+#include "debug_hack.hpp"
+#endif
+
 class case_loop {
 public:
-    void read_and_solve() {
-	std::cin >> T;
-	for(int t=1; t<=T; ++t) {
-	    read();
-	    solve();
-	    std::cout << "Case #" << t << ":";
-	}
+  case_loop() = default;
+  void read_and_solve() {
+    std::cin >> T;
+    for (int t = 1; t <= T; ++t) {
+      read();
+      solve();
+      std::cout << "Case #" << t << ":";
     }
+  }
+
 private:
-    void read() {
-	;
-    }
-    void solve() {
-	;
-    }
+  void read() { ; }
+  void solve() { ; }
+
 private:
-    int T;
+  int T;
 };
 
-int main() {
-    case_loop main_loop;
-    
+int main(int argc, char **argv) {
+#ifdef DEBUG_HACK
+  dh::debug_hack(argv);
+#endif
+  case_loop main_loop;
+  main_loop.read_and_solve();
 }
