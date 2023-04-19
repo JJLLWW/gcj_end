c9dbeb04fc5ad83e6ba3824340d4eb76507a3647
Jack Wright <jackwright@Jacks-MacBook-Pro.local>
2023-04-19 21:09:53 +0100

    better boilerplate

--- RoundA/task1.cpp
+++ RoundA/task1.cpp
@@ -1,7 +1,8 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
 #include <string>
-#include <array>
-#include <unordered_set>
+#include <vector>
 
 #ifdef DEBUG_HACK
 #include "debug_hack.hpp"
@@ -11,62 +12,67 @@
 // D[c - 'A'] gives digit word mapped to.
 // return YES if are 2 different words with same encoding.
 
-// As words are distinct can check if the computed encoding has been seen before?
+// As words are distinct can check if the computed encoding has been seen
+// before?
 // - needs to be O(N).
 
-// If all strings at most 10 characters may be enough memory to just store converted
-// representations as strings in an unordered set.
+// If all strings at most 10 characters may be enough memory to just store
+// converted representations as strings in an unordered set.
 
 // slight problem is always need to read all N strings.
 
 class case_loop {
 public:
-	case_loop() = default;
-    void read_and_solve() {
-		std::cin >> T;
-		for(int t=1; t<=T; ++t) {
-			bool is_yes {read()};
-			std::cout << "Case #" << t << ": ";
-			std::cout << ((is_yes) ? "YES" : "NO") << std::endl;
-		}
+  case_loop() = default;
+  void read_and_solve() {
+    std::cin >> T;
+    for (int t = 1; t <= T; ++t) {
+      read();
+      std::cout << "Case #" << t << ": " << solve() << std::endl;
     }
+  }
+
 private:
-    bool read() {
-		seen.clear();
-		for(int i=0; i<26; ++i) {
-			std::cin >> D[i];
-		}
-		std::cin >> N;
-		std::string S;
-		for(int i=0; i<N; ++i) {
-			std::cin >> S;
-			S = encode(S);
-			if(seen.count(S) > 0) {
-				// BAD - doesn't read the rest of the line.
-				return true;
-			}
-			seen.insert(S);
-		}
-		return false;
-	}
-	std::string encode(const std::string& raw_str) {
-		std::string enc_str;
-		for(char c : raw_str) {
-			enc_str.push_back('0' + D[c - 'A']);
-		}
-		return enc_str;
+  void read() {
+    for (int i = 0; i < 26; ++i) {
+      std::cin >> D[i];
     }
+    std::cin >> N;
+    encoded.resize(N);
+    std::string S;
+    for (int i = 0; i < N; ++i) {
+      std::cin >> S;
+      encoded[i] = encode(S);
+    }
+  }
+  std::string solve() {
+    std::sort(encoded.begin(), encoded.end());
+    for (int i = 0; i < N - 1; ++i) {
+      if (encoded[i] == encoded[i + 1]) {
+        return "YES";
+      }
+    }
+    return "NO";
+  }
+  std::string encode(const std::string &raw_str) {
+    std::string enc_str;
+    for (char c : raw_str) {
+      enc_str.push_back('0' + D[c - 'A']);
+    }
+    return enc_str;
+  }
+
 private:
-    int T;
-    int N;
-    std::unordered_set<std::string> seen;
-    std::array<int, 26> D;
+  int T;
+  int N;
+  std::vector<std::string> encoded;
+  std::array<int, 26> D;
 };
 
 int main(int argc, char **argv) {
-	#ifdef DEBUG_HACK
-	dh::debug_hack(argv);
-	#endif
-    case_loop main_loop;
-    main_loop.read_and_solve();
+#ifdef DEBUG_HACK
+  dh::debug_hack(argv);
+#endif
+  case_loop main_loop;
+  main_loop.read_and_solve();
 }
--- RoundA/task2.cpp
+++ RoundA/task2.cpp
@@ -1,20 +1,11 @@
+#include <cstring>
+#include <filesystem>
 #include <iostream>
 #include <stdio.h>
-#include <cstring>
 #include <string>
-#include <filesystem>
 
 #ifdef DEBUG_HACK
 #include "debug_hack.hpp"
 #endif
 
-int main(int argc, char **argv) {
-    #ifdef DEBUG_HACK
-    dh::debug_hack(argv);
-    #endif
-    int N;
-    std::cin >> N;
-    N += 1;
-    std::cout << N << std::endl;
-    int M = N;
-}
+int main(int argc, char **argv) { ; }
